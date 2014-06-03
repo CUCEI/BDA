763dd3a05146f911eb6614b36bfb164c1dcb3be7
Cesar Landeros <cesar.landeros@corb.mx>
2014-06-03 03:33:52 -0500

    finished testing, starting the good stuf =)

--- ajax/clientes.php
+++ ajax/clientes.php
@@ -2,6 +2,33 @@
 
 $consulta = new mysqli('localhost', 'root', '1234', 'sarka');
 
-$resultado = $consulta->query('select * from departamento where id_departamento = 1');
+if( $_GET['llamada'] == 'consulta' ){
+
+	$resultado = $consulta->query('select * from cliente;');
+
+	while ( $datos_raw = $resultado->fetch_assoc() ) {
+		$datos[] = $datos_raw;
+	}
+	echo json_encode($datos);
+}
+else if( $_GET['llamada'] == 'inserta' ){
+	$resultado = $consulta->query('select count(id_cliente) as num from cliente;');
+
+	$datos = $resultado->fetch_assoc();
+
+	$id_cliente = $datos['num']; 
+
+	$resultado = $consulta->query("insert into cliente values('" . $id_cliente ."','" . $_GET['nombre'] ."','" . $_GET['telefono'] . "','" . $_GET['direccion'] . "','" . $_GET['nombre_contacto'] . "','" . $_GET['RFC'] . "');");
+	echo 'insert into cliente values(' . $id_cliente .',' . $_GET['nombre'] .',' . $_GET['telefono'] . ',,, ' . $_GET['RFC'] . ');';
+	$datos = $resultado->fetch_assoc();
+	echo json_encode($datos);
+	foreach ( $datos as $key=>$value) {
+		echo $key;
+	}
+}
+
+/*
+
 
 var_dump( $resultado->fetch_assoc() );
+*/
--- js/sarka.js
+++ js/sarka.js
@@ -1,4 +1,31 @@
 $(document).ready(function () {
-	$("header").load('/template/menu.html');
-	
+	$("header").load('/template/menu.html', function(){
+		seccion = $('header').attr('class');
+		$('nav').find('li.' + seccion.split(' ')[1]).addClass('active')
+	});
+	$('#consulta').click(function(event) {
+
+		$.getJSON( "ajax/clientes.php?llamada=consulta", function( data ) {
+
+		var items = [], main = $("main"), i=0, textohtml = '';
+
+		textohtml+= '<table class="table">';
+		
+		for( i=0; i < data.length; i++){
+			textohtml+= '<tr>';
+
+		  	textohtml+= '<td id="">' + data[i].id_cliente + '</td>';
+		  	textohtml+= '<td id="">' + data[i].nombre + '</td>';
+		  	textohtml+= '<td id="">' + data[i].telefono + '</td>';
+		  	textohtml+= '<td id="">' + data[i].direccion + '</td>';
+		  	textohtml+= '<td id="">' + data[i].nombre_contacto + '</td>';
+		  	textohtml+= '<td id="">' + data[i].rfc + '</td>';
+
+		    textohtml+= '</tr>';
+		}
+		 	
+		textohtml+= '</table>';
+		main.append(textohtml);
+		});
+	});
 });
